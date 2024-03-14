c20ccd7b9b29b312cf41098707db9a6b1773424f
neonforgedevbot <devbot@neonforge.com>
2024-03-14 15:39:07 -0700

    update: neonversion.go

--- staging/src/k8s.io/kubectl/pkg/cmd/neon/neonversion.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/neon/neonversion.go
@@ -18,7 +18,7 @@ package neon_utility
 
 // GetNeonCliVersion returns the associated neon-cli version.
 func GetNeonCliVersion() string {
-    return "0.11.0-alpha.0"
+    return "0.11.0"
 }
 
 // GetHelmVersion returns the associated Helm version.
